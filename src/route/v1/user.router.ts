fc43ada65e54739cdd48c2cbd543edacdfe710e0
harsh <harshsondagar@weetechsolution.com>
2026-06-22 17:18:33 +0530

    final commiit
    feat : added full  user
    
    feat : added jwt uils
    
    feat: added user reposetoreis
    
    feat: added abuild & start script

--- src/route/v1/user.router.ts
+++ src/route/v1/user.router.ts
@@ -2,23 +2,23 @@ import router, { Request, Response } from "express"
 
 const userRouter = router()
 
-userRouter.get("/", (req: Request, res: Response) => {
-    
+userRouter.get("/:id", (req: Request, res: Response) => {
+
     res.status(200).json({
         message: "Hello! welcome to server",
-        
+  
     })
-
 })
 
 userRouter.post("/", (req: Request, res: Response) => {
     
     res.status(200).json({
-        message: "Hello! ",
+
         
-    })
+        message: "Hello! welcome ",
+    })   
 
 })
 
 
-export default userRouter
+export default userRouter   
