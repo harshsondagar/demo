d664c9b8d16b9af1239aef22dd68e87deba5e366
harsh <harshsondagar@weetechsolution.com>
2026-06-22 17:30:12 +0530

    added a usermessage

--- src/route/v1/user.router.ts
+++ src/route/v1/user.router.ts
@@ -3,7 +3,6 @@ import router, { Request, Response } from "express"
 const userRouter = router()
 
 userRouter.get("/", (req: Request, res: Response) => {
-
     res.status(200).json({
         message: "Hello! user",
     })
