337f8fc6f1a4b81976a12720000b8111ccb23c95
harsh <harshsondagar@weetechsolution.com>
2026-06-22 15:04:19 +0530

    feat : added current chganges

--- src/route/v1/authRouter.ts
+++ src/route/v1/authRouter.ts
@@ -2,10 +2,23 @@ import router from "express"
 
 const authRouter = router()
 
-authRouter.post("/register",()=>{
-     
+authRouter.post("/register", (req: Request, res: Response) => {
+    const {username, password} = req.body
+
+    if(!username || !password || password < 6){
+        res.status(401).json({
+            message: "incorrect username and password"
+        })
+        return
+    }
+
+    // TODO : make sure user not exist
+    // TODO : store username and hash password in db!
+
+    res.status(201).json({
+        message: "register successfully",
+        username
+    })
 })
-
-
 
 export default authRouter
