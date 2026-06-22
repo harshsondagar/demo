bee29d630b6b8300667c1268f39ccfe83bea19cf
harsh <harshsondagar@weetechsolution.com>
2026-06-22 15:02:40 +0530

    feat : added a todo

--- src/route/v1/authRouter.ts
+++ src/route/v1/authRouter.ts
@@ -1,59 +1,13 @@
-import router, { Request, Response } from "express"
+import router from "express"
 
 const authRouter = router()
 
-authRouter.post("/register",(req:Request,res:Response)=>{
-    const {username,password} = req.body
-
-    if(!username || !password || password < 6){
-        res.status(401).json({
-            message : "incorrect username and password"
-        })
-        return
-    }
-
-    // TODO : make sure user not exsist
-
-    // TODO : store username and hash password  in db!
-
-    res.status(201).json({
-        message : "register successfully",
-        username
-    })
-
-})
-
-<<<<<<< HEAD
 authRouter.post("/register",()=>{
-    // TODO : signin
-=======
-
-authRouter.post("/register",(req:Request,res:Response)=>{
-    const {username,password} = req.body
-
-    if(!username || !password || password < 6){
-        res.status(401).json({
-            message : "incorrect username and password"
-        })
-        return
-    }
-
-    // TODO : make sure user exist
-
-    // TODO : store token in cookie 
-
-    res.status(201).json({
-        message : "login successfully",
-        username
-    })
+    // TODO : register logic
 })
 
-authRouter.post("/logout",(req:Request,res:Response)=>{
-    res.status(201).json({
-        message : "logout successfully",
+authRouter.post("/register",()=>{
 
-    })
->>>>>>> feature/auth
 })
 
 export default authRouter
